52590955eb30e8e1af8cf274d2d856950527f369
Viktor Gal <viktor.gal@maeth.com>
2014-07-10 14:41:00 +0800

    Porting example to use zmq

--- 01.java/01.mahout/01.example/itembasedrec.sh
+++ 01.java/01.mahout/01.example/itembasedrec.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # chkconfig: 2345 95 20
 # description: Mahout recommendation algo example
-# 
+#
 # processname: itembasedrec
 
 case $1 in
     start)
-	cd /mnt/algo
-	java -cp target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp /mnt/messaging/ > /tmp/mahout.log &
+        cd /mnt/algo
+        ORCH=`netstat -rn | grep "^0.0.0.0 " | cut -d " " -f10`
+        java -cp target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp tcp://$ORCH:2760 > /tmp/mahout.log &
     ;;
     stop)
-      pid=`ps aux | grep dev.crowdrec.recs.mahout.ItembasedRec_batch | grep java |awk '{print $2}'`
-      kill -9 $pid
+        pid=`ps aux | grep dev.crowdrec.recs.mahout.ItembasedRec_batch | grep java |awk '{print $2}'`
+        kill -9 $pid
     ;;
 esac
 exit 0
--- 01.java/01.mahout/01.example/src/main/java/dev/crowdrec/recs/mahout/ItembasedRec_batch.java
+++ 01.java/01.mahout/01.example/src/main/java/dev/crowdrec/recs/mahout/ItembasedRec_batch.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
@@ -17,8 +18,14 @@ import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMsg;
+import org.zeromq.ZFrame;
+
 public class ItembasedRec_batch {
-	
+
 	private static final String OUTMSG_READY = "READY";
 	private static final String OUTMSG_OK = "OK";
 	private static final String OUTMSG_KO = "KO";
@@ -27,200 +34,123 @@ public class ItembasedRec_batch {
 	private static final String TRAIN_CMD = "TRAIN";
 	private static final String READINPUT_CMD = "READ_INPUT";
 	private static final String STOP_CMD = "STOP";
-	
-	private static final String READINPUT_RELATIONS = "relations";
-	private static final String READINPUT_ENTITIES = "entities";
-	
-	private static final String CMD_OUT_FILENAME = "cmd_out.msg";
-	private static final String CMD_IN_FILENAME = "cmd_in.msg";
-	
+
 	private static final String TMP_MAHOUT_USERRATINGS_FILENAME = "mahout_ratings.csv";
 	private static final boolean INPUT_FILE_HAS_HEADER = true;
-	
-	private static final long SLEEP_MSECS = 5000l;
-	
+
 	private String stagedir = null;
-	private String commdir = null;
-	
+	private Socket communication_socket = null;
+
 	/**
-	 * 
+	 *
 	 * @param args
 	 * $0: stage directory: directory where the algorithm can persist data (e.g., temp files, models,..)
 	 * $1: communication directory: directory reserved to communication messages
-	 * @throws IOException 
-	 * @throws NumberFormatException 
-	 * @throws TasteException 
+	 * @throws IOException
+	 * @throws NumberFormatException
+	 * @throws TasteException
 	 */
 	public static void main(String[] args) throws NumberFormatException, IOException, TasteException {
-		
+
 		if ( args.length < 2 ) {
 			System.out.println("missing parameters");
 			return;
 		}
-		
+
 		String outdir = args[0];
-		String communicationdir = args[1];
-		
-		ItembasedRec_batch ubr = new ItembasedRec_batch(outdir,communicationdir);
+		String comm = args[1];
+
+		Context context = ZMQ.context(1);
+		Socket comm_sock = context.socket(ZMQ.REQ);
+		comm_sock.connect(comm);
+
+		ItembasedRec_batch ubr = new ItembasedRec_batch(outdir, comm_sock);
 		ubr.run();
+
+		comm_sock.close();
+		context.term();
 	}
-	
-	public ItembasedRec_batch(String stagedir, String commdir) throws IOException {
+
+	public ItembasedRec_batch(String stagedir, Socket socket) throws IOException {
 		this.stagedir = stagedir;
-		this.commdir = commdir;
-		
-		FileWriter writer = null;
-		File msg_out = new File(commdir + File.separator + CMD_OUT_FILENAME);
-		try {
-			writer = new FileWriter(msg_out);
-			writer.write(OUTMSG_READY);
-		} finally {
-			if ( writer != null ) {
-				writer.close();
-			}
-		}
+		this.communication_socket = socket;
+
 		System.out.println("ALGO: machine started");
 	}
-	
+
 	public void run() throws IOException, TasteException {
 		Recommender recommender = null;
 		boolean stop = false;
+
+		communication_socket.send(OUTMSG_READY, 0);
 		while ( !stop ) {
-			File msg_in = new File(commdir + File.separator + CMD_IN_FILENAME);
-			File msg_out = new File(commdir + File.separator + CMD_OUT_FILENAME);
-			if ( msg_in.exists() ) {
+			ZMsg recvMsg = ZMsg.recvMsg(this.communication_socket);
+			ZFrame command = recvMsg.remove();
+
+			if (command.streq(READINPUT_CMD)) {
+				System.out.println("ALGO: running READ INPUT cmd");
+				boolean success = cmdReadinput(recvMsg);
+
+				System.out.println(success ? "ALGO: input correctly read" : "ALGO: failing input read");
+				communication_socket.send(success ? OUTMSG_OK : OUTMSG_KO);
+			} else if (command.streq(TRAIN_CMD)) {
+				System.out.println("ALGO: running TRAIN cmd");
 				try {
-					Thread.sleep(SLEEP_MSECS); // wait some seconds for the file to be written
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+					recommender = createRecommender(stagedir + File.separator + TMP_MAHOUT_USERRATINGS_FILENAME);
+
+					System.out.println("ALGO: recommender created");
+					communication_socket.send(OUTMSG_OK);
+				} catch (TasteException e) {
+					communication_socket.send(OUTMSG_KO);
 				}
-				
-				BufferedReader msgreader = null;
-				FileWriter writer = null;
-				try {
-					msgreader = new BufferedReader(new FileReader(msg_in));
-					String command = msgreader.readLine();
-					if ( READINPUT_CMD.equals(command) ) {
-						System.out.println("ALGO: running READ INPUT cmd");
-						boolean success = cmdReadinput(msgreader);
-						try {
-							System.out.println(success ? "ALGO: input correctly read" : "ALGO: failing input read");
-							writer = new FileWriter(msg_out);
-							writer.write(success ? OUTMSG_OK : OUTMSG_KO);
-						} finally {
-							if ( writer != null ) {
-								writer.close();
-							}
-						}
-					} else if (TRAIN_CMD.equals(command) ) {
-						System.out.println("ALGO: running TRAIN cmd");
-						try {
-							recommender = createRecommender(stagedir + File.separator + TMP_MAHOUT_USERRATINGS_FILENAME);
-							try {
-								System.out.println("ALGO: recommender created");
-								writer = new FileWriter(msg_out);
-								writer.write(OUTMSG_OK);
-							} finally {
-								if ( writer != null ) {
-									writer.close();
-								}
-							}
-						} catch (TasteException e) {
-							try {
-								writer = new FileWriter(msg_out);
-								writer.write(OUTMSG_KO);
-								e.printStackTrace();
-							} finally {
-								if ( writer != null ) {
-									writer.close();
-								}
-							}
-						}
-					} else if (RECOMMEND_CMD.equals(command)) {
-						System.out.println("ALGO: running RECOMMEND cmd");
-						boolean success = (recommender != null && cmdRecommend(msgreader, recommender));
-						try {
-							System.out.println(success ? "ALGO: recommedation completed correctly" : "ALGO: failure in generating recommendations");
-							writer = new FileWriter(msg_out);
-							writer.write(success ? OUTMSG_OK : OUTMSG_KO);
-						} finally {
-							if ( writer != null ) {
-								writer.close();
-							}
-						}
-					} else if (STOP_CMD.equals(command)) {
-						try {
-							writer = new FileWriter(msg_out);
-							writer.write(OUTMSG_OK);
-							stop = true;
-						} finally {
-							if ( writer != null ) {
-								writer.close();
-							}
-						}
-					}
-				} finally {
-					if ( msgreader != null ) {
-						msgreader.close();
-					}
-					if ( writer != null ) {
-						writer.close();
-					}
-				}
-				System.out.println("ALGO: cmd executed. Deleting msg");
-				msg_in.delete();
+			} else if (command.streq(RECOMMEND_CMD)) {
+				System.out.println("ALGO: running RECOMMEND cmd");
+				ZMsg recomms = null;
+				boolean success = (recommender != null && (recomms = cmdRecommend(recvMsg, recommender)).size() > 0 ) ;
+
+				System.out.println(success ? "ALGO: recommedation completed correctly" : "ALGO: failure in generating recommendations");
+				if (success)
+					recomms.addFirst(OUTMSG_OK);
+				else
+					recomms.addFirst(OUTMSG_KO);
+
+				recomms.send(communication_socket);
+			} else if (command.streq(STOP_CMD)) {
+				communication_socket.send(OUTMSG_OK);
+				stop = true;
 			} else {
-				try {
-					Thread.sleep(SLEEP_MSECS);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				System.out.println("ALGO: unknown command");
 			}
+
 		}
 		System.out.println("shutdown");
 	}
-	
-	protected boolean cmdRecommend(BufferedReader reader, Recommender recommender) throws IOException, TasteException {
-		String out_filename = reader.readLine();
-		BufferedWriter writer =  null;
-		try {
-			File writer_file = new File( commdir + File.separator + out_filename);
-			writer = new BufferedWriter(new FileWriter(writer_file));
-			String line = null;
-			while ((line=reader.readLine())!=null) {
-				int userid = Integer.parseInt(line);
-				writer.append("BEGIN user " + userid + "\n");
-				List<RecommendedItem> reclist = recommender.recommend(userid, 5);
-				if ( reclist != null && reclist.size() > 0 ) {
-					for ( RecommendedItem item : reclist ) {
-						writer.append(item.toString() + "\n");
-					}
+
+	protected ZMsg cmdRecommend(ZMsg msg, Recommender recommender) throws IOException, TasteException {
+		ZMsg recomms = new ZMsg();
+		for (ZFrame userIdStr : msg) {
+			int userid = Integer.parseInt(userIdStr.toString());
+			recomms.addString("BEGIN user " + userid);
+			List<RecommendedItem> reclist = recommender.recommend(userid, 5);
+			if ( reclist != null && reclist.size() > 0 ) {
+				for ( RecommendedItem item : reclist ) {
+					recomms.addString(item.toString());
 				}
-				writer.append("END user " + userid + "\n");
 			}
-		} finally {
-			if ( writer != null ) {
-				writer.close();
-			}
+			recomms.addString("END user " + userid);
 		}
-		return true;
+
+		return recomms;
 	}
-	
-	protected boolean cmdReadinput(BufferedReader reader) throws IOException {
-		String entities_filename = null;
-		String relations_filename = null;
-		String line = null;
-		while ((line=reader.readLine())!=null) {
-			String[] els = line.split("=");
-			if ( els.length == 2 ) {
-				String type = els[0].trim();
-				String val = els[1].trim();
-				if ( READINPUT_ENTITIES.equals(type) ) 
-					entities_filename = val;
-				if ( READINPUT_RELATIONS.equals(type) )
-					relations_filename = val;
-			}
+
+	protected boolean cmdReadinput(ZMsg msg) throws IOException {
+		if (msg.size() != 2) {
+			// wrong number of elements
+			return false;
 		}
+
+		String entities_filename = msg.remove().toString();
+		String relations_filename = msg.remove().toString();
 		if ( entities_filename != null && relations_filename != null ) {
 			convertDataset(stagedir, entities_filename, relations_filename, "user","movie","rating.explicit");
 			return true;
@@ -229,14 +159,14 @@ public class ItembasedRec_batch {
 			return false;
 		}
 	}
-	
+
 	protected Recommender createRecommender(String filename) throws IOException, TasteException{
 		DataModel model = new FileDataModel(new File(filename));
 		ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
 		Recommender recommender = new GenericItemBasedRecommender(model, similarity);
 		return recommender;
 	}
-	
+
 	protected void convertDataset(String outdir, String entities_filename, String relations_filename, String user_etype, String movie_etype, String rating_rtype) throws NumberFormatException, IOException {
 		BufferedReader relations_reader = null;
 		BufferedWriter ratings_writer = null;
@@ -251,17 +181,17 @@ public class ItembasedRec_batch {
 			while ( (line = relations_reader.readLine()) != null ) {
 				String[] els = line.split("\t");
 				String rtype = els[0];
-				
+
 				if ( rtype.equals(rating_rtype) ) {
 					String rid = els[1];
 					long ts = Long.parseLong( els[2] );
 					String props = els[3];
 					String links = els[4];
-					
+
 					String userid = null;
 					String itemid = null;
 					double ratingscore = 0;
-					
+
 					if ( props != null ) {
 						String[] els_props = props.split("::");
 						for ( String el_props : els_props ) {
@@ -272,9 +202,9 @@ public class ItembasedRec_batch {
 							}
 						}
 					}
-					
+
 					if ( links != null ) {
-						String[] els_links = links.split("::"); 
+						String[] els_links = links.split("::");
 						for ( String el_links : els_links ) {
 							el_links = el_links.replaceAll("\\((.*)\\)", "$1");
 							String mdname = el_links.split("=")[0];
@@ -310,6 +240,6 @@ public class ItembasedRec_batch {
 				ratings_writer.close();
 			}
 		}
-		
+
 	}
 }
