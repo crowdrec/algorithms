90e4514cae0d64e329ff58d1e14f378f1658e68d
Roberto Turrin <roberto.turrin@moviri.com>
2014-07-30 10:32:32 +0200

    updated ZQM itembasedrec.sh

--- 01.java/01.mahout/01.example/itembasedrec.sh
+++ 01.java/01.mahout/01.example/itembasedrec.sh
@@ -9,15 +9,18 @@ echo "itembasedrec service" $1 >> $LOG
 
 case $1 in
     status)
-        $0 start
+        status=`ps aux | grep dev.crowdrec.recs.mahout.ItembasedRec_batch | grep java | wc -l`
+        echo status $status >> $LOG
+        #$0 start
+        #exit 0
     ;;
     start)
 	    echo "starting itembasedrec service" >> $LOG
-        sleep 20
+        #sleep 30
         cd /mnt/algo
         ORCH=`netstat -rn | grep "^0.0.0.0 " | cut -d " " -f10`
         echo executing java -cp /mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp tcp://$ORCH:2760 >> $LOG
-        java -cp /mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp tcp://$ORCH:2760 >> $LOG
+        java -cp /mnt/algo/target/crowdrec-mahout-test-1.0-SNAPSHOT-jar-with-dependencies.jar dev.crowdrec.recs.mahout.ItembasedRec_batch /tmp tcp://$ORCH:2760 >> $LOG &
     ;;
     stop)
 	   echo "stopping itembasedrec service" >> $LOG
